ec468d2ac8a36c8a3ca8f358eef0210269d1e6b0
jakobfriberg <jakob.friberg@bontouch.com>
2023-03-20 20:45:28 +0100

    edit

--- scripts/two_shot.py
+++ scripts/two_shot.py
@@ -171,7 +171,7 @@ class Script(scripts.Script):
 
                 #run_button.click(fn=do_visualize, inputs=[input_image,detect_resolution,image_resolution], outputs=[])
                 
-                run_button.click(fn=self.do_visualize, inputs=[input_image, module, processor_res, threshold_a, threshold_b], outputs=[generated_image])
+                run_button.click(fn=self.do_visualize, inputs=[self, input_image, module, processor_res, threshold_a, threshold_b], outputs=[generated_image])
 
         unit = gr.State(default_unit)
 
